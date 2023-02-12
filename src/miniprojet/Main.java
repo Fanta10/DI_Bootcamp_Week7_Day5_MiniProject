6a8d81b6ad99b3124246df821ce20298fb5c15f4
Fanta10 <fantakamate107@gmail.com>
2023-02-12 15:56:31 +0000

    commit1

---
+++ src/miniprojet/Main.java
@@ -0,0 +1,11 @@
+package miniprojet;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 new Question().play();
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HP
+ *
+ */
+module DI_Bootcamp_Week7_Day5_MiniProject {
+}
